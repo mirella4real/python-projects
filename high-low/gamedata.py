fa31c4167d70713962ee18f61847f4bf7b78abf9
Mirella Batista <mirelbatista@gmail.com>
2024-09-11 18:03:52 -0400

    adding data source for high-low game

---
+++ high-low/gamedata.py
@@ -0,0 +1,176 @@
+data = [
+    {  
+        'name': 'Mark Cuban',
+        'follower_count': 2100000,
+        'description': 'an American businessman and investor',
+        'country': 'United States'
+    },
+    {
+        'name': 'New York Times',
+        'follower_count': 18300000,
+        'description': 'a daily newspaper',
+        'country': 'United States'       
+    },
+    {  
+        'name': 'Wiser Than Me',
+        'follower_count': 35200,
+        'description': 'a podcast',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Kamala Harris',
+        'follower_count': 18200000,
+        'description': 'a presidential candidate',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Washington Post',
+        'follower_count': 6800000,
+        'description': 'a daily newspaper',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Adam Grant',
+        'follower_count': 2500000,
+        'description': 'an organizational psychologist and author',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Barack Obama',
+        'follower_count': 56000000,
+        'description': 'a former president',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Jono Pandolfi Designs',
+        'follower_count': 140000,
+        'description': 'a ceramic artist and designer',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Marvel Studios',
+        'follower_count': 37600000,
+        'description': 'a movie studio',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Kevin Bacon',
+        'follower_count': 4400000,
+        'description': 'an actor',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Snow Patrol',
+        'follower_count': 284000,
+        'description': 'an indie rock band',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Bobbi Brown',
+        'follower_count': 868000,
+        'description': 'a makeup artist',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Jones Road Beauty',
+        'follower_count': 513000,
+        'description': 'a makeup brand',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Politico',
+        'follower_count': 1900000,
+        'description': 'a news website',
+        'country': 'United States'
+    },
+    {  
+        'name': 'The Daily Show',
+        'follower_count': 10300000,
+        'description': 'a late-night talk and satirical news TV program',
+        'country': 'United States'
+    },
+    {  
+        'name': 'The Female Quotient',
+        'follower_count': 2900000,
+        'description': 'a media company',
+        'country': 'United States'
+    },
+    {  
+        'name': 'The Late Show',
+        'follower_count': 2500000,
+        'description': 'a late-night talk show',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Salma Hayek',
+        'follower_count': 28900000,
+        'description': 'an actress',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Instagram',
+        'follower_count': 676000000,
+        'description': 'a social media platform',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Apple',
+        'follower_count': 33700000,
+        'description': 'a technology company',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Billie Eilish',
+        'follower_count': 119000000,
+        'description': 'a singer',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Dua Lipa',
+        'follower_count': 87600000,
+        'description': 'a singer',
+        'country': 'UK'
+    },
+    {  
+        'name': 'Playbill',
+        'follower_count': 902000,
+        'description': ' a sews and media company',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Zendaya',
+        'follower_count': 182000000,
+        'description': 'an actress',
+        'country': 'United States'
+    },
+    {  
+        'name': 'JLo',
+        'follower_count': 251000000,
+        'description': 'a singer, actress and business woman',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Ryan Reynolds',
+        'follower_count': 54100000,
+        'description': 'an actor and businessman',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Taylor Swift',
+        'follower_count': 283000000,
+        'description': 'a singer and businesswoman',
+        'country': 'United States'
+    },
+    {  
+        'name': 'Hugh Jackman',
+        'follower_count': 34500000,
+        'description': 'an actor and businessman',
+        'country': 'United States'
+    },
+    {  
+        'name': 'National Geographic',
+        'follower_count': 281000000,
+        'description': 'a monthly magazine',
+        'country': 'United States'
+    },
+]
