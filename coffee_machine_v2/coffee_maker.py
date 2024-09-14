97dd1b440406e51ce6b0eaee9dc3b513d3058022
Mirella Batista <mirelbatista@gmail.com>
2024-09-14 19:52:11 -0400

    Initial classes for OO version of coffee machine

---
+++ coffee_machine_v2/coffee_maker.py
@@ -0,0 +1,35 @@
+from prettytable import PrettyTable
+class CoffeeMaker:
+
+    def __init__(self):
+        self.type_of = "CoffeeMaker"
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+
+    def get_type_of(self):
+        return self.type_of
+    
+    def get_resource_amount(self, resource):
+        return self.resources[resource]   
+    
+    def report(self):
+        table = PrettyTable()
+        table.add_column("Resource", ['water', 'milk', 'coffee'])
+        table.add_column("Amount", [self.get_resource_amount('water'), self.get_resource_amount('milk'), self.get_resource_amount('coffee')])
+        table.align = "l"
+        return table
+    
+    def is_resource_sufficient(self, drink):
+        can_make = True
+        for item in drink.ingredients:
+            if drink.ingredients[item] > self.resources[item]:
+                can_make = False
+        return can_make
+    
+    def make_coffee(self, order):
+        for item in order.ingredients:
+            self.resources[item] -= order.ingredients[item]
+        return True
---
+++ coffee_machine_v2/menu.py
@@ -0,0 +1,55 @@
+class Menu:
+
+    def __init__(self):
+        type_of = "Menu"
+        self.menu = [
+            MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+            MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
+            MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3)
+        ]
+
+    def get_type_of(self):
+        return self.type_of
+    
+    def get_items(self):
+        """Returns the names of all available items in the menu"""
+        options = ""
+        for item in self.menu:
+            options += item.name
+            if self.menu.index(item) < len(self.menu) -1:
+                options += "/"
+        return options
+    
+    def find_drink(self, order_name):
+        menu_item = MenuItem(name="", water=0, milk=0, coffee=0, cost=2.0)
+        for item in self.menu:
+             if item.name == order_name:
+                 menu_item = item
+        return menu_item
+                    
+
+
+
+class MenuItem:
+
+    def __init__(self, name, water, milk, coffee, cost):
+        self.type_of = "MenuItem"
+        self.name = name
+        self.cost = cost
+        self.ingredients = {
+            "water": water,
+            "milk": milk,
+            "coffee": coffee
+        }
+
+    def get_type_of(self):
+        return self.type_of
+    
+    def get_name(self):
+        return self.name
+    
+    def get_cost(self):
+        return self.cost
+    
+    def get_ingredients(self):
+        return self.ingredients
