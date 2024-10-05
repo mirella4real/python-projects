133fdffde666573b9d142cd7cc4f50d0a6edac7a
Mirella Batista <mirelbatista@gmail.com>
2024-10-04 20:29:37 -0400

    Fixed mistake in ball class

--- pong_game/ball.py
+++ pong_game/ball.py
@@ -6,24 +6,25 @@ class Ball(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.ball = Turtle("circle")
-        self.ball.penup()
-        self.ball.shapesize(1,1)
-        self.ball.color("white")
+        self.shape("circle")
+        self.penup()
+        self.shapesize(1,1)
+        self.color("white")
         self.y_move = MOVE_DISTANCE
         self.x_move = MOVE_DISTANCE
-        self.ball.setpos(0,0)
+        self.setpos(0,0)
+       
 
     def reset_ball(self):
-        self.ball.setpos(0,0)
+        self.setpos(0,0)
         self.bounce_x()
 
     def move_ball(self):
-        old_x = self.ball.xcor()
-        old_y = self.ball.ycor()
+        old_x = self.xcor()
+        old_y = self.ycor()
         new_x = old_x + self.x_move
         new_y = old_y + self.y_move
-        self.ball.setpos(new_x, new_y)
+        self.setpos(new_x, new_y)
 
     def bounce_y(self):
         self.y_move *= -1
@@ -32,10 +33,10 @@ class Ball(Turtle):
         self.x_move *= -1
 
     def get_x(self):
-        return self.ball.xcor()
+        return self.xcor()
     
     def get_y(self):
-        return self.ball.ycor()
+        return self.ycor()
     
     def get_distance(self, target):
-        return self.ball.distance(target)
+        return self.distance(target)
