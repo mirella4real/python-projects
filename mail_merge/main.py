849b9d583f49805f01a5d43052cd189931f590fa
Mirella Batista <mirelbatista@gmail.com>
2024-10-11 22:33:59 -0400

    Created a placeholder constant

--- mail_merge/main.py
+++ mail_merge/main.py
@@ -1,12 +1,5 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+PLACEHOLDER = "[Name]"
 
 with open("./Input/Letters/starting_letter.txt", mode="r") as letter_file:
     letter_template = letter_file.read()
@@ -18,7 +11,7 @@ with open("./Input/Names/invited_names.txt", mode="r") as name_file:
         names_list.append(name.strip('\n'))
 
 for name in names_list:  
-    letter = letter_template.replace("[Name]", name) 
+    letter = letter_template.replace(PLACEHOLDER, name) 
     with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as new_letter:
         new_letter.write(letter)
     
