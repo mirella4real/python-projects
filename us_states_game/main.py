92d6161946e65fe8de896cd82cb82ca830a521ce
Mirella Batista <mirelbatista@gmail.com>
2024-10-13 20:43:34 -0400

    loaded background image

---
+++ us_states_game/main.py
@@ -0,0 +1,10 @@
+import turtle
+
+my_screen = turtle.Screen()
+my_screen.title("US States Game")
+image = "blank_states_img.gif"
+my_screen.addshape(image)
+turtle.shape(image)
+
+
+my_screen.exitonclick()
