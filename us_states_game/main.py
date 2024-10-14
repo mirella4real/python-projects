37386f375c823331308f65e08d69093f79075bd2
Mirella Batista <mirelbatista@gmail.com>
2024-10-13 20:50:08 -0400

    Added a prmpt box to game

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -1,10 +1,13 @@
 import turtle
 
+PROMPT_TITLE = "How Many States Can You Name?"
+PROMPT_TEXT = "Enter a state name"
 my_screen = turtle.Screen()
 my_screen.title("US States Game")
 image = "blank_states_img.gif"
 my_screen.addshape(image)
 turtle.shape(image)
 
+answer_state = my_screen.textinput(title=PROMPT_TITLE, prompt=PROMPT_TEXT).lower()
 
 my_screen.exitonclick()
