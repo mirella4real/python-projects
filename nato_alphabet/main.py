915384f82dc022a54a54ce14c13e5e59de5a5a7c
Mirella Batista <mirelbatista@gmail.com>
2024-10-22 21:16:07 -0400

    Converted each letter of user input into phonetic code

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -3,7 +3,10 @@ import pandas
 # TODO 1. Create dictionary in format: { "A":"Alfa", "B":"Bravo"}
 
 phonetic_alpha_df = pandas.read_csv("./nato_phonetic_alphabet.csv")
-phonetic_alpha_dict = {row[0]:row[1] for (index,row) in phonetic_alpha_df.iterrows()}
-print(phonetic_alpha_dict)
+phonetic_alpha_dict = {row.letter:row.code for (index,row) in phonetic_alpha_df.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word the user inputs
+
+user_input = input("Enter a word you would like translated to the Nato phonetic alphabet: \n").upper()
+program_output = [phonetic_alpha_dict[letter] for letter in user_input if letter in phonetic_alpha_dict]
+print(program_output)
