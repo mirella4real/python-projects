45d4acfc28afe45fbe2d6fb7f1b6e1866d79ea43
Mirella Batista <mirelbatista@gmail.com>
2024-10-22 21:26:59 -0400

    Added line breaks to output

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -6,7 +6,7 @@ phonetic_alpha_df = pandas.read_csv("./nato_phonetic_alphabet.csv")
 phonetic_alpha_dict = {row.letter:row.code for (index,row) in phonetic_alpha_df.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word the user inputs
-
-user_input = input("Enter a word you would like translated to the Nato phonetic alphabet: \n").upper()
+line_breaks = "\n\n\n\n\n"
+user_input = input(f"{line_breaks}Enter a word you would like translated to the Nato phonetic alphabet: \n").upper()
 program_output = [phonetic_alpha_dict[letter] for letter in user_input if letter in phonetic_alpha_dict]
-print(program_output)
+print(f"\nTranslation: {program_output}{line_breaks}")
