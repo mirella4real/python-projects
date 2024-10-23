2bfd30b610a534ad73bc7e0f904936021003c996
Mirella Batista <mirelbatista@gmail.com>
2024-10-22 20:55:02 -0400

    Create a dictionary from dataframe

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -1,1 +1,9 @@
 import pandas
+
+# TODO 1. Create dictionary in format: { "A":"Alfa", "B":"Bravo"}
+
+phonetic_alpha_df = pandas.read_csv("./nato_phonetic_alphabet.csv")
+phonetic_alpha_dict = {row[0]:row[1] for (index,row) in phonetic_alpha_df.iterrows()}
+print(phonetic_alpha_dict)
+
+# TODO 2. Create a list of the phonetic code words from a word the user inputs
