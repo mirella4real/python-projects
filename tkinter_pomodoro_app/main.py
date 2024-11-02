d71be29d264be38f6b42b3ac97e4fa53668691bb
Mirella Batista <mirelbatista@gmail.com>
2024-11-02 10:35:57 -0400

    Added tomato image and countdown text

--- tkinter_pomodoro_app/main.py
+++ tkinter_pomodoro_app/main.py
@@ -9,6 +9,8 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+timer_text = "00:00"
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -21,6 +23,11 @@ timer_window = Tk()
 timer_window.title("Pomodoro")
 timer_window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_canvas = Canvas(width=206, height=224, bg=YELLOW, highlightthickness=0)
+tomtato_img = PhotoImage(file="./tomato.png")
+timer_canvas.create_image(100, 112, image=tomtato_img)
+timer_canvas.create_text(100, 130, text=timer_text, fill="white", font=(FONT_NAME, 35, "bold"))
+timer_canvas.pack()
 
 
 # equivalent of a while loop
