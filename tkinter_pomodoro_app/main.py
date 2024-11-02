e5714dbae6b981f8550ca95951f0f02cd33be1f1
Mirella Batista <mirelbatista@gmail.com>
2024-11-02 10:18:21 -0400

    Initial setup for pomodoro project

---
+++ tkinter_pomodoro_app/main.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+timer_window = Tk()
+timer_window.title("Pomodoro")
+
+
+
+# equivalent of a while loop
+timer_window.mainloop()
