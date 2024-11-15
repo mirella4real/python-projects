3c16b93525b2a9ab65fef3ea96f3a31b61f878d2
Mirella Batista <mirelbatista@gmail.com>
2024-11-15 07:54:56 -0500

    Added constants for strings, colors and more

--- tkinter_password_manager/main.py
+++ tkinter_password_manager/main.py
@@ -1,15 +1,20 @@
 from tkinter import *
 
-TITLE = "Password Manager"
-WHITE = "#ffffff"
-
-
+STR_TITLE = "Password Manager"
+STR_WEBSITE = "Website"
+STR_EMAI_UNAME = "Email/Username"
+STR_PWD = "Password"
+STR_GEN_PWD = "Generate Password"
+STR_ADD = "Add"
+COLOR_WHITE = "#ffffff"
+COLOR_BLACK = "#000000"
+FONT_NAME = "Courier"
 
 app_window = Tk()
-app_window.title(TITLE)
-app_window.config(padx=20, pady=20, bg=WHITE)
+app_window.title(STR_TITLE)
+app_window.config(padx=20, pady=20, bg=COLOR_WHITE)
 
-app_canvas = Canvas(width=200, height=200, bg=WHITE, highlightthickness=0)
+app_canvas = Canvas(width=200, height=200, bg=COLOR_WHITE, highlightthickness=0)
 app_logo_image = PhotoImage(file="./logo.png")
 app_canvas.create_image(100, 102, image=app_logo_image)
 app_canvas.grid(column=1, row=1)
