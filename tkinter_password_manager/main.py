fa42360ffa434c76a853935fc1c8cc50fa8ae11f
Mirella Batista <mirelbatista@gmail.com>
2024-11-17 10:21:00 -0500

    Added default email and updated alignment of items

--- tkinter_password_manager/main.py
+++ tkinter_password_manager/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+COMMONLY_USED_EMAIL = "myemail@email.com"
 STR_TITLE = "Password Manager"
 STR_WEBSITE = "Website"
 STR_EMAIL_UNAME = "Email/Username"
@@ -21,28 +22,30 @@ app_canvas.grid(column=1, row=0)
 
 # website
 label_website = Label(text=STR_WEBSITE, font=(FONT_NAME, 20, "normal"), fg=COLOR_BLACK, bg=COLOR_WHITE)
-label_website.grid(column=0, row=1)
+label_website.grid(column=0, row=1, sticky="NW")
 input_website = Entry(width=35)
-input_website.grid(column=1, row=1, columnspan=2)
+input_website.focus()
+input_website.grid(column=1, row=1, columnspan=2, sticky="NW")
 
 # email/username
 label_email_username = Label(text=STR_EMAIL_UNAME, font=(FONT_NAME, 20, "normal"), fg=COLOR_BLACK, bg=COLOR_WHITE)
-label_email_username.grid(column=0, row=2)
+label_email_username.grid(column=0, row=2, sticky="NW")
 input_email_username = Entry(width=35)
-input_email_username.grid(column=1, row=2, columnspan=2)
+input_email_username.insert(0, string=COMMONLY_USED_EMAIL)
+input_email_username.grid(column=1, row=2, columnspan=2, sticky="NW")
 
 # password
 label_pwd = Label(text=STR_PWD, font=(FONT_NAME, 20, "normal"), fg=COLOR_BLACK, bg=COLOR_WHITE)
-label_pwd.grid(column=0, row=3)
+label_pwd.grid(column=0, row=3, sticky="NW")
 input_pwd = Entry(width=21)
-input_pwd.grid(column=1, row=3)
+input_pwd.grid(column=1, row=3, sticky="NW")
 
 # generate password
 button_gen_pwd = Button(text=STR_GEN_PWD, highlightbackground=COLOR_WHITE)
-button_gen_pwd.grid(column=2, row=3)
+button_gen_pwd.grid(column=2, row=3, sticky="NW")
 
 # add password
 button_add = Button(text=STR_ADD, highlightbackground=COLOR_WHITE, width=36)
-button_add.grid(column=1, row=4, columnspan=2)
+button_add.grid(column=1, row=4, columnspan=2, sticky="NW")
 
 app_window.mainloop()
