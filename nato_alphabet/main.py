ea40f02e82774b7a4761b70add749a1d1bc9a507
Mirella Batista <mirelbatista@gmail.com>
2024-11-24 14:55:13 -0500

    Added error handling to and option to continue translation

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -1,12 +1,29 @@
 import pandas
 
-# TODO 1. Create dictionary in format: { "A":"Alfa", "B":"Bravo"}
-
+# Create dictionary in format: { "A":"Alfa", "B":"Bravo"}
 phonetic_alpha_df = pandas.read_csv("./nato_phonetic_alphabet.csv")
 phonetic_alpha_dict = {row.letter:row.code for (index,row) in phonetic_alpha_df.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word the user inputs
-line_breaks = "\n\n\n\n\n"
-user_input = input(f"{line_breaks}Enter a word you would like translated to the Nato phonetic alphabet: \n").upper()
-program_output = [phonetic_alpha_dict[letter] for letter in user_input if letter in phonetic_alpha_dict]
-print(f"\nTranslation: {program_output}{line_breaks}")
+# Create a list of the phonetic code words from a word the user inputs
+line_breaks = "\n\n"
+run_program = True
+while run_program:
+    
+    user_input = input(f"{line_breaks}Enter a word you would like translated to the Nato phonetic alphabet: \n").upper()
+    # program_output = [phonetic_alpha_dict[letter] for letter in user_input if letter in phonetic_alpha_dict]
+
+    try:
+        program_output = [phonetic_alpha_dict[letter] for letter in user_input]
+    except KeyError:
+        program_output = []
+
+    if len(program_output) == 0:
+        print(f"\nTranslation: No translation available for the word you entered.{line_breaks}")
+    else:
+        print(f"\nTranslation: {program_output}{line_breaks}")
+        
+    user_continue = input("Would you like to translate another word? Type 'yes' or 'no': ").lower()
+    if user_continue == "yes":
+        run_program = True
+    else:
+        run_program = False
