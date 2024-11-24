1f0a8ea5717ad0b90d49b299a1fa99d7d39e3fac
Mirella Batista <mirelbatista@gmail.com>
2024-11-24 15:01:50 -0500

    Refactored code

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -6,25 +6,17 @@ phonetic_alpha_dict = {row.letter:row.code for (index,row) in phonetic_alpha_df.
 
 # Create a list of the phonetic code words from a word the user inputs
 line_breaks = "\n\n"
-run_program = True
-while run_program:
+
+def generate_phonetic():
     
     user_input = input(f"{line_breaks}Enter a word you would like translated to the Nato phonetic alphabet: \n").upper()
-    # commenting this out and saving alternate: it validates the input and only translates the letters that are in the dictionary
-    # program_output = [phonetic_alpha_dict[letter] for letter in user_input if letter in phonetic_alpha_dict]
-
+    
     try:
         program_output = [phonetic_alpha_dict[letter] for letter in user_input]
     except KeyError:
-        program_output = []
-
-    if len(program_output) == 0:
-        print(f"\nTranslation: No translation available for the word you entered.{line_breaks}")
+        print("No translation available for the word you entered.")
+        generate_phonetic()
     else:
         print(f"\nTranslation: {program_output}{line_breaks}")
-        
-    user_continue = input("Would you like to translate another word? Type 'yes' or 'no': ").lower()
-    if user_continue == "yes":
-        run_program = True
-    else:
-        run_program = False
+
+generate_phonetic()
