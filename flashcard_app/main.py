efdb6f4f128791a67d8752de73a87aa72babf2c3
Mirella Batista <mirelbatista@gmail.com>
2024-12-01 21:34:21 -0500

    Loaded data from csv and displayed a random word on button press

--- flashcard_app/main.py
+++ flashcard_app/main.py
@@ -1,9 +1,19 @@
 from tkinter import *
+import pandas
+import random
 
 STR_TITLE = "Flashy"
 BACKGROUND_COLOR = "#B1DDC6"
 FONT= "Ariel"
 
+# Import data using pandas library
+words = pandas.read_csv("./data/french_words.csv")
+words_dictionary = words.to_dict(orient="records")
+
+# Display new random word
+def get_random_word():
+    random_word = random.choice(list(words_dictionary))
+    app_canvas.itemconfig(card_term, text=random_word["French"])
 # Create the UI using Tkinter
 app_window = Tk()
 app_window.title(STR_TITLE)
@@ -17,10 +27,10 @@ card_term = app_canvas.create_text(400, 263, text="trouve", fill="black", font=(
 app_canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightbackground=BACKGROUND_COLOR, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightbackground=BACKGROUND_COLOR, highlightthickness=0, command=get_random_word)
 wrong_button.grid(column=0, row=1)
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightbackground=BACKGROUND_COLOR, highlightthickness=0)
+right_button = Button(image=right_image, highlightbackground=BACKGROUND_COLOR, highlightthickness=0, command=get_random_word)
 right_button.grid(column=1, row=1)
 app_window.mainloop()
