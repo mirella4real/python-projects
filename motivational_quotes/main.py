efd2e6d3c42094bf4c1a290ee80ce437a4b66dc0
Mirella Batista <mirelbatista@gmail.com>
2024-12-11 21:01:34 -0500

    loading quotes from txt file and picked a random quote

--- motivational_quotes/main.py
+++ motivational_quotes/main.py
@@ -1,8 +1,26 @@
 import datetime as dt
+import random
 
 now = dt.datetime.now()
 day_of_the_week = now.weekday()
+quotes_list = []
+
+def load_quotes():
+    global quotes_list
+    try:
+        with open("./quotes.txt", "r") as file:
+            quotes_list = [line.strip() for line in file]
+    except:
+      quotes_list.append("All out of motivation today - Mirella Batista")
+
+def get_random_quote():
+    quote = ""
+    if len(quotes_list) == 0:
+        load_quotes()
+    quote = random.choice(quotes_list)
+    return quote
 
 #wednesday is 2
 if day_of_the_week == 2:
-    print("today!")
+    quote_of_the_day = get_random_quote()
+    print(quote_of_the_day)
