c90c69a321630f6ed4c1d8fc8b184070b3f5761d
Mirella Batista <mirelbatista@gmail.com>
2024-12-31 13:01:50 -0500

    Integrated Open Trivia api call and removed hard-coded data source

--- quizzler_app/main.py
+++ quizzler_app/main.py
@@ -1,6 +1,7 @@
 from question_model import Question # type: ignore
-from data import question_data
 from quiz_brain import QuizBrain
+import requests
+
 CORRECT = "Correct!"
 WRONG = "Wrong answer."
 YOUR_CURRENT_SCORE = "Your current score is"
@@ -9,10 +10,24 @@ CORRECT_ANSWER = "The correct answer was:"
 END_MESSAGE = "You've completed the quiz!"
 FINAL_SCORE = "Your final score was:"
 question_bank = []
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+def call_api(api_url, parameters=None):
+    if parameters != None:
+        response = requests.get(url=api_url, params=parameters)
+    else:
+        response = requests.get(url=api_url)
+    response.raise_for_status()
+    return response.json()
 
 def create_question_bank():
-    for question in question_data:
-        question_bank.append(Question(question['text'], question['answer']))
+    data = call_api("https://opentdb.com/api.php", parameters)
+
+    for question in data["results"]:
+        question_bank.append(Question(question['question'], question['correct_answer']))
     return True
 
 def run_quiz(my_quiz_brain, question):
