df9c77ddcfc8be47620a665d4cbca039e08ba3f2
Mirella Batista <mirelbatista@gmail.com>
2025-01-01 18:19:28 -0500

    Display current score in the UI

--- quizzler_app/ui.py
+++ quizzler_app/ui.py
@@ -5,6 +5,7 @@ THEME_COLOR = "#375362"
 BACKGROUND_COLOR = "#ffffff"
 FONT = ("Arial", 20, "italic")
 FONT_SCORE = ("Arial", 14, "normal")
+SCORE_LABEL = "Score:"
 
 class QuizInterface:
 
@@ -15,7 +16,7 @@ class QuizInterface:
         self.app_window.title("Quizzler")
         self.app_window.config(padx=20, pady=20, background=THEME_COLOR)
 
-        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR, font=FONT_SCORE)
+        self.score_label = Label(text=f"{SCORE_LABEL} 0", fg="white", bg=THEME_COLOR, font=FONT_SCORE)
         self.score_label.grid(row=0, column=1, sticky="nsew")
 
         self.app_canvas = Canvas(width=300, height=250, bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -28,7 +29,7 @@ class QuizInterface:
             font=FONT
         )
         self.app_canvas.grid(row=1, column=0, columnspan=2, pady=30)
-        self.display_next_question()
+        self.show_new_question()
 
         self.true_image = PhotoImage(file="./images/true.png")
         self.true_button = Button(image=self.true_image, highlightbackground=BACKGROUND_COLOR, highlightthickness=0, command=self.true_pressed)
@@ -55,9 +56,11 @@ class QuizInterface:
     def check_answer(self, response):
         is_correct = self.quiz.check_answer(response)
         self.give_feedback(is_correct)
-        self.app_window.after(1000, self.display_next_question)
+        self.app_window.after(1000, self.show_new_question)
 
-    def display_next_question(self):
+    def show_new_question(self):
+        score = self.quiz.get_score()
+        self.score_label.config(text=f"{SCORE_LABEL} {score}")
         self.app_canvas.config(bg="#ffffff")
         question = self.quiz.next_question()
         self.app_canvas.itemconfig(self.question_text, text=f"Q.{question.number}: {question.text}")
