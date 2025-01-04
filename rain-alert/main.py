97a65da509a4f96d2dd1456246585b6f3dd97021
Mirella Batista <mirelbatista@gmail.com>
2025-01-04 12:11:13 -0500

    Refactored and added functionality to send whatsapp message from Twilio

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -1,14 +1,8 @@
-from config import API_KEY, MY_LAT, MY_LONG
+from config import *
+from twilio.rest import Client
 import requests
 
-API_URL = "https://api.openweathermap.org/data/2.5/forecast"
 
-parameters = {
-    "lat": MY_LAT,
-    "lon": MY_LONG,
-    "appid": API_KEY,
-    "cnt": 4
-}
 def call_api(api_url, parameters=None):
     if parameters != None:
         response = requests.get(url=api_url, params=parameters)
@@ -18,14 +12,29 @@ def call_api(api_url, parameters=None):
     return response.json()
 
 def rain_in_forecast()->bool:
-    five_day_forecast = call_api(API_URL, parameters)
+    parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": OPENWEATHER_API_KEY,
+    "cnt": 4
+}
+    five_day_forecast = call_api(OPENWEATHERMAP_ENDPOINT, parameters)
     will_rain = False
     for forecast in five_day_forecast["list"]:
         if forecast["weather"][0]["id"] < 700:
             will_rain = True
     return will_rain
 
+def send_sms(sms_message):
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_TOKEN)
+    message = client.messages.create(
+        body=sms_message,
+        from_=whatsapp_sender_number,
+        to=whatsapp_recipient_number,
+    )
+    print(message.status)
+
 if rain_in_forecast() == True:
-    print("Bring an umbrella.")
+    send_sms(f"It's going to rain today in {LOCATION}. Remember to bring an ☔️")
 else:
     print("Leave the umbrella at home.")
