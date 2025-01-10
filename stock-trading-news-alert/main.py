ad6f92faf617dfd7bde6a4e4bb0480515e21bb67
Mirella Batista <mirelbatista@gmail.com>
2025-01-09 20:56:41 -0500

    Added API call to NewsAPI

--- stock-trading-news-alert/main.py
+++ stock-trading-news-alert/main.py
@@ -1,10 +1,12 @@
 from config import *
+from datetime import date, timedelta
 from test_data import *
 import requests
 IS_TEST = True
 STOCK = "IBM"
 COMPANY_NAME = "International Business Machines (IBM)"
 ALPHAVANTAGE_ENDPOINT = "https://www.alphavantage.co/query"
+NEWSAPI_ENDPOINT = "https://newsapi.org/v2/everything"
 
 def call_api(api_url, parameters=None):
     if parameters != None:
@@ -13,6 +15,7 @@ def call_api(api_url, parameters=None):
         response = requests.get(url=api_url)
     response.raise_for_status()
     return response.json()
+
 def compute_percentage_difference(final:float, initial:float)->float:
     percentage_change = (final-initial)/initial * 100
     return percentage_change
@@ -44,9 +47,25 @@ def get_closing_status(stock_data)->dict:
         closing_status["status"] = "Decrease"
     return closing_status
 
+def get_related_news():
+    from_date = date.today() - timedelta(days=1)
+    parameters = {
+        "q": "IBM",
+        "from": from_date,
+        "language": "en",
+        "apiKey": NEWSAPI_KEY
+    }
+    if IS_TEST:
+        news_response = NEWSAPI_MOCK_DATA
+    else:
+        news_response = call_api(NEWSAPI_ENDPOINT, parameters)
+    return news_response
+
 
 stock_data = get_stock_data()
 closing_status = get_closing_status(stock_data)
 if closing_status["difference"] >= 5:
-    print("Good news!")
-    
+    stock_news = get_related_news()
+
+
+
