e0628162724154a0b5a6e1738eb5a9bfd31a33f8
Mirella Batista <mirelbatista@gmail.com>
2025-01-14 20:54:53 -0500

    functionality for updating a pixel in the graph

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,20 +1,36 @@
 import requests
+from datetime import datetime
 from config import *
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 PIXELA_GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{PIXELA_USERNAME}/graphs"
 
-graph_config = {
-    "id": "graph1",
-    "name": "Studying Graph",
-    "unit": "minutes",
-    "type": "float",
-    "color": "sora"
-}
-
 headers = {
     "X-USER-TOKEN": PIXELA_TOKEN
 }
 
-response = requests.post(url=PIXELA_GRAPH_ENDPOINT, json=graph_config, headers=headers)
-print(response.text)
+def create_a_graph():
+    graph_config = {
+        "id": "graph1",
+        "name": "Studying Graph",
+        "unit": "minutes",
+        "type": "float",
+        "color": "sora"
+    }
+    response = requests.post(url=PIXELA_GRAPH_ENDPOINT, json=graph_config, headers=headers)
+    print(response.text)
+
+def post_a_pixel():
+    today = datetime.today()
+    yyyyMMdd_date = today.strftime("%Y%m%d")
+    post_pixel = {
+        "date": yyyyMMdd_date,
+        "quantity": "90"
+    }
+    response = requests.post(url=f"{PIXELA_GRAPH_ENDPOINT}/graph1", json=post_pixel, headers=headers)
+    print(response.text)
+
+post_a_pixel()
+
+
+
