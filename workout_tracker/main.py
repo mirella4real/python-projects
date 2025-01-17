76061e8b0fc3d2f9341027d5d10fd02d10882d27
Mirella Batista <mirelbatista@gmail.com>
2025-01-16 21:22:01 -0500

    Queried the Nutritionix API and printed response

--- workout_tracker/main.py
+++ workout_tracker/main.py
@@ -2,3 +2,22 @@ import requests
 from config import *
 HOST_DOMAIN = "https://trackapi.nutritionix.com"
 API_ENDPOINT = "/v2/natural/exercise"
+
+headers = {
+    "x-app-id" : APP_ID,
+    "x-app-key": API_KEY
+}
+
+def get_exercise_data(exercise_txt:str):
+    parameters = {
+        "query": exercise_txt,
+        "gender": GENDER,
+        "weight_kg": WEIGHT,
+        "height_cm": HEIGHT,
+        "age": AGE
+    }
+    response = requests.post(url=f"{HOST_DOMAIN}{API_ENDPOINT}", json=parameters, headers=headers)
+    print(response.text)
+
+exercise_text = input("Tell me which exercises you did:")
+get_exercise_data(exercise_text)
