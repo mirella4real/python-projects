751d7353e3ef393a2ec2fdfe844e001300e5184f
Mirella Batista <mirelbatista@gmail.com>
2025-01-16 22:25:05 -0500

    Added functionality to parse response data and api call to save to google sheet

--- workout_tracker/main.py
+++ workout_tracker/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 from config import *
 HOST_DOMAIN = "https://trackapi.nutritionix.com"
 API_ENDPOINT = "/v2/natural/exercise"
@@ -17,7 +18,30 @@ def get_exercise_data(exercise_txt:str):
         "age": AGE
     }
     response = requests.post(url=f"{HOST_DOMAIN}{API_ENDPOINT}", json=parameters, headers=headers)
+    return response.text
+
+def parse_exercise_parameters(response_data):
+    exercise = response_data["exercises"][0]["name"]
+    today_date = datetime.today().strftime("%d/%m/%Y")
+    now_time = datetime.now().strftime("%I:%M %p")
+    duration = response_data["exercises"][0]["duration_min"]
+    kcals = response_data["exercises"][0]["nf_calories"]
+    parameters = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise,
+            "duration": duration,
+            "calories": "259.3"
+        }
+    }
+    return parameters
+
+def save_workout(parameters):
+    response = requests.post(url=SHEETY_API_ENDPOINT, json=parameters, headers=SHEETY_HEADER)
     print(response.text)
 
-exercise_text = input("Tell me which exercises you did:")
-get_exercise_data(exercise_text)
+# exercise_text = input("Tell me which exercises you did:")
+# respose = get_exercise_data(exercise_text)
+
+save_workout(parse_exercise_parameters(TEST_RESPONSE))
