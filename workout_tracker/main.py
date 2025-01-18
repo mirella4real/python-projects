650d334dd9dccedae4b1a9d4ab118b95ce3a60c4
Mirella Batista <mirelbatista@gmail.com>
2025-01-18 17:06:50 -0500

    Added environment variables

--- workout_tracker/main.py
+++ workout_tracker/main.py
@@ -1,12 +1,23 @@
 import requests
+import os
 from datetime import datetime
 from config import *
 HOST_DOMAIN = "https://trackapi.nutritionix.com"
 API_ENDPOINT = "/v2/natural/exercise"
 
+def save_environment_vars():
+    os.environ["APP_ID"] = APP_ID
+    os.environ["API_KEY"] = API_KEY
+
+def get_app_id():
+    return os.environ.get("APP_ID")
+
+def get_api_key():
+    return os.environ.get("API_KEY")
+
 headers = {
-    "x-app-id" : APP_ID,
-    "x-app-key": API_KEY
+    "x-app-id" : get_app_id(),
+    "x-app-key": get_api_key()
 }
 
 def get_exercise_data(exercise_txt:str):
@@ -18,6 +29,7 @@ def get_exercise_data(exercise_txt:str):
         "age": AGE
     }
     response = requests.post(url=f"{HOST_DOMAIN}{API_ENDPOINT}", json=parameters, headers=headers)
+    # print(response.text)
     return response.json()
 
 def parse_exercise_parameters(response_data):
@@ -40,6 +52,7 @@ def parse_exercise_parameters(response_data):
 def save_workout(parameters):
     response = requests.post(url=SHEETY_API_ENDPOINT, json=parameters, headers=SHEETY_HEADER)
 
+save_environment_vars()
 exercise_text = input("Tell me which exercises you did:")
 response = get_exercise_data(exercise_text)
 save_workout(parse_exercise_parameters(response))
