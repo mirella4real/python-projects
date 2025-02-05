8eabd78107b5d013da0138037c2c28fe924e7448
Mirella Batista <mirelbatista@gmail.com>
2025-02-04 20:24:27 -0500

    Imported pprint

--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,9 +1,10 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from config import *
 import requests
+import pprint
 
 def get_worksheet_data():
     response = requests.get(url=SHEETY_API_ENDPOINT, headers=SHEETY_HEADER)
     return response.json()
 
-print(get_worksheet_data())
+pprint.pprint(get_worksheet_data())
