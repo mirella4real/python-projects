c74796e551a58ac924654abaf3f9d8466e999d24
Mirella Batista <mirelbatista@gmail.com>
2025-02-04 20:19:36 -0500

    Test request using Sheety API - print content in Google sheet

--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,1 +1,9 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from config import *
+import requests
+
+def get_worksheet_data():
+    response = requests.get(url=SHEETY_API_ENDPOINT, headers=SHEETY_HEADER)
+    return response.json()
+
+print(get_worksheet_data())
