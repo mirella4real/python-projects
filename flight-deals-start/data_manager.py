587f5dbe4c17ce9dab48c441c08af4415b28670b
Mirella Batista <mirelbatista@gmail.com>
2025-02-06 19:36:22 -0500

    Added test data for development

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -1,19 +1,32 @@
 from config import *
 import requests
 
+TEST_DATA = [{'city': 'Paris', 'iataCode': 'h', 'id': 2, 'lowestPrice': 54},
+ {'city': 'Frankfurt', 'iataCode': '', 'id': 3, 'lowestPrice': 42},
+ {'city': 'Tokyo', 'iataCode': '', 'id': 4, 'lowestPrice': 485},
+ {'city': 'Hong Kong', 'iataCode': '', 'id': 5, 'lowestPrice': 551},
+ {'city': 'Istanbul', 'iataCode': '', 'id': 6, 'lowestPrice': 95},
+ {'city': 'Kuala Lumpur', 'iataCode': '', 'id': 7, 'lowestPrice': 414},
+ {'city': 'New York', 'iataCode': '', 'id': 8, 'lowestPrice': 240},
+ {'city': 'San Francisco', 'iataCode': '', 'id': 9, 'lowestPrice': 260},
+ {'city': 'Dublin', 'iataCode': '', 'id': 10, 'lowestPrice': 378}]
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     def __init__(self):
         pass
 
     def get_worksheet_data(self):
-        try:
-            response = requests.get(url=SHEETY_API_ENDPOINT, headers=SHEETY_HEADER)
-            response.raise_for_status() 
-            data = response.json()
-            return data["prices"]
-        except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
-            return None
+        if IS_TEST != True:
+            try:
+                response = requests.get(url=SHEETY_API_ENDPOINT, headers=SHEETY_HEADER)
+                response.raise_for_status() 
+                data = response.json()
+                return data["prices"]
+            except requests.exceptions.RequestException as e:
+                print(f"An error occurred: {e}")
+                return None
+        else:
+            return TEST_DATA
     
 
--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -3,6 +3,5 @@ import pprint
 from data_manager import DataManager
 
 flight_data = DataManager()
-
 sheet_data = flight_data.get_worksheet_data()
 pprint.pprint(sheet_data)
