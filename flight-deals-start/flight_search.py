e33a3f95f895dcdcee8847f57482f5e35dc6c6ba
Mirella Batista <mirelbatista@gmail.com>
2025-02-10 21:27:29 -0500

    retrieved amadeus api config

--- flight-deals-start/flight_search.py
+++ flight-deals-start/flight_search.py
@@ -1,7 +1,11 @@
+from config import *
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     def __init__(self):
-        pass
+        self._api_key = get_amadeus_api_key()
+        self._api_secret = get_amadeus_api_secret()
+        self._api_token = get_amadeus_api_token()
 
     def get_iata_code(self, city:str)->str:
         return "TESTING"
